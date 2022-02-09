bc3d24b74419d6db3942fb9e9eafcc52988cdd16
Prashansa.s <prashansa.shanbhag@relevancelab.com>
2022-02-09 19:14:39 +0530

    Update makestudies.sh

--- makestudies.sh
+++ makestudies.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-$ yq eval '.' -o=json $1 | jq -r '{description:.Description,tags:.Tags,bookmarkedBy:[],projectId:[],levelOfSharing:.0,sharedTo:[],name:.Name,studyType:"Public",resourceDetails:.Resources|map({description:.Description,arn:.ARN,region:.Region,type:.Type|select(.=="S3 Bucket")|"s3"}),moreInformation:.License,__v:0,isDeleted:false,repositoryName:"Registry of Open Data on AWS"}'
+yq -o=json eval '.' "$1" | jq -r '{description:.Description,tags:.Tags,bookmarkedBy:[],projectId:[],levelOfSharing:.0,sharedTo:[],name:.Name,studyType:"Public",resourceDetails:.Resources|map({description:.Description,arn:.ARN,region:.Region,type:.Type|select(.=="S3 Bucket")|"s3"}),moreInformation:.License,__v:0,isDeleted:false,repositoryName:"Registry of Open Data on AWS"}'
 
